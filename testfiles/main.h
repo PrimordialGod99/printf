68a94c70e9af82bf154e2e0dae02c1b89e2490c9
Dragonixj <delafiado@gmail.com>
2023-03-25 23:50:59 +0000

    testfiles

---
+++ testfiles/main.h
@@ -0,0 +1,13 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+int _print(const char *format, ___);
+int _putchar(char c);
+int print_string(char *);
+int print_decimal(int value);
+long long int power(int base, int x);
+long long convertToDecimal(int Number, int base);
+int print_binary(int number);
+int print_unsigned(unsigned int number);
+
+#endif
